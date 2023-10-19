d4545ee19f6fa60b8dfefad5d2e134fc37de3c41
mukminpitoyo <mpitoyo.2020@scis.smu.edu.sg>
2023-10-19 09:50:17 +0800

    cleaning up Terraform config files

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "codepipeline_codedeploy_role" {
-  name = "tsy_iabs_codepipeline_codedeploy_iam_role"
+resource "aws_iam_role" "tsy_iabs_iam_role" {
+  name = "tsy_iabs_iam_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -61,43 +61,43 @@ resource "aws_iam_role" "codepipeline_codedeploy_role" {
 resource "aws_iam_policy_attachment" "codepipeline_policy_attachment" {
   name       = "MyCodePipelinePolicyAttachment"
   policy_arn = "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess"
-  roles      = [aws_iam_role.codepipeline_codedeploy_role.name]
+  roles      = [aws_iam_role.tsy_iabs_iam_role.name]
 }
 
 resource "aws_iam_policy_attachment" "codedeploy_policy_attachment" {
   name       = "MyCodeDeployPolicyAttachment"
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployFullAccess"
-  roles      = [aws_iam_role.codepipeline_codedeploy_role.name]
+  roles      = [aws_iam_role.tsy_iabs_iam_role.name]
 }
 
 resource "aws_iam_policy_attachment" "s3_policy_attachment" {
   name       = "MyS3PolicyAttachment"
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-  roles      = [aws_iam_role.codepipeline_codedeploy_role.name]
+  roles      = [aws_iam_role.tsy_iabs_iam_role.name]
 }
 
 resource "aws_iam_policy_attachment" "ec2_policy_attachment" {
   name       = "MyCodeBuildPolicyAttachment"
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-  roles      = [aws_iam_role.codepipeline_codedeploy_role.name]
+  roles      = [aws_iam_role.tsy_iabs_iam_role.name]
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_managed_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  role       = aws_iam_role.codepipeline_codedeploy_role.name
+  role       = aws_iam_role.tsy_iabs_iam_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "elastic_beanstalk_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess-AWSElasticBeanstalk"
-  role       = aws_iam_role.codepipeline_codedeploy_role.name
+  role       = aws_iam_role.tsy_iabs_iam_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "rds_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
-  role       = aws_iam_role.codepipeline_codedeploy_role.name
+  role       = aws_iam_role.tsy_iabs_iam_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "cloudwatch_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
-  role       = aws_iam_role.codepipeline_codedeploy_role.name
+  role       = aws_iam_role.tsy_iabs_iam_role.name
 }
--- main.tf
+++ main.tf
@@ -231,7 +231,7 @@ resource "aws_security_group" "elb" {
 # Creating Instance Profile
 resource "aws_iam_instance_profile" "tsy_iabs_instance_profile" {
   name = "tsy-iabs-instance-profile"
-  role = aws_iam_role.codepipeline_codedeploy_role.id
+  role = aws_iam_role.tsy_iabs_iam_role.id
 }
 
 # Creating ACM Certificate for HTTPS
@@ -243,7 +243,6 @@ resource "aws_acm_certificate" "tsy_iabs_certificate" {
     create_before_destroy = true
   }
 }
-
 
 # Creating RDS Subnet Group
 resource "aws_db_subnet_group" "tsy_iabs_db_subnet_group" {
@@ -259,7 +258,6 @@ resource "aws_db_instance" "tsy_iabs_db_instance" {
   engine                = "mysql"
   engine_version        = "8.0"
   instance_class        = "db.t3.micro"
-  name                  = "tsy_db"
   username = jsondecode(data.aws_secretsmanager_secret_version.rds_secrets.secret_string)["DB_USERNAME"]
   password = jsondecode(data.aws_secretsmanager_secret_version.rds_secrets.secret_string)["DB_PASSWORD"]
   publicly_accessible  = true
@@ -278,183 +276,4 @@ resource "aws_db_instance" "tsy_iabs_db_instance" {
   tags = {
     Name = "tsy-iabs-db-instance"
   }
-}
-
-# Defining the Elastic Beanstalk TSY-IABS Application
-resource "aws_elastic_beanstalk_application" "tsy_iabs_app" {
-  name        = "tsy-iabs-be-app"
-  description = "TSY-IABS TheStrengthYard-CMS-Service Application"
-}
-
-# # Defining the Elastic Beanstalk TSY-IABS Environment
-resource "aws_elastic_beanstalk_environment" "tsy_iabs_env" {
-  name                = "tsy-iabs-env"
-  application         = aws_elastic_beanstalk_application.tsy_iabs_app.name
-  solution_stack_name = "64bit Amazon Linux 2023 v4.0.1 running Docker"
-
-  # Configuring Elastic Beanstalk env with necessary settings
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "VPCId"
-    value     = aws_vpc.tsy-iabs-vpc.id
-  }
-
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "Subnets"
-    value     = "${join(",", 
-    [aws_subnet.private_a.id], 
-    [aws_subnet.private_b.id])}"
-  }
-
-  # Pass RDS connection details to Elastic Beanstalk environment
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "DB_HOSTNAME"
-    value     = aws_db_instance.tsy_iabs_db_instance.endpoint
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "DB_PORT"
-    value     = aws_db_instance.tsy_iabs_db_instance.port
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "DB_NAME"
-    value     = aws_db_instance.tsy_iabs_db_instance.name
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "DB_USERNAME"
-    value     = aws_db_instance.tsy_iabs_db_instance.username
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "DB_PASSWORD"
-    value     = aws_db_instance.tsy_iabs_db_instance.password
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "SECURITY_PASSWORD_SALT"
-    value     = jsondecode(data.aws_secretsmanager_secret_version.email_secrets.secret_string)["SECURITY_PASSWORD_SALT"]
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "EMAIL_USER"
-    value     = jsondecode(data.aws_secretsmanager_secret_version.email_secrets.secret_string)["EMAIL_USER"]
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "EMAIL_PASSWORD"
-    value     = jsondecode(data.aws_secretsmanager_secret_version.email_secrets.secret_string)["EMAIL_PASSWORD"]
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "PAYPAL_CLIENT_ID"
-    value     = jsondecode(data.aws_secretsmanager_secret_version.paypal_secrets.secret_string)["PAYPAL_CLIENT_ID"]
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "PAYPAL_CLIENT_SECRET"
-    value     = jsondecode(data.aws_secretsmanager_secret_version.paypal_secrets.secret_string)["PAYPAL_CLIENT_SECRET"]
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "OPENAI_API_KEY"
-    value     = jsondecode(data.aws_secretsmanager_secret_version.chatbot_secrets.secret_string)["OPENAI_API_KEY"]
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "LANGCHAIN_API_KEY"
-    value     = jsondecode(data.aws_secretsmanager_secret_version.chatbot_secrets.secret_string)["LANGCHAIN_API_KEY"]
-  }
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = aws_iam_instance_profile.tsy_iabs_instance_profile.name
-  }
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "SecurityGroups"
-    value     = aws_security_group.instance.id
-  }
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "EC2KeyName"
-    value     = "tsy-iabs-keypair"
-  }
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "InstanceType"
-    value     = "t2.micro"
-  }
-
-  setting {
-    namespace = "aws:autoscaling:asg"
-    name      = "MinSize"
-    value     = "1"
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:environment"
-    name      = "LoadBalancerType"
-    value     = "classic"
-  }
-
-  # Turn on Listener for HTTPS
-  setting {
-    namespace = "aws:elb:listener:443"
-    name      = "ListenerEnabled"
-    value     = "true"
-  }
-
-  # Configure ACM Certificate for HTTPS
-  setting {
-    namespace = "aws:elb:listener:443"
-    name      = "SSLCertificateId"
-    value     = aws_acm_certificate.tsy_iabs_certificate.arn
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:environment"
-    name      = "EnvironmentType"
-    value     = "LoadBalanced"
-  }
-
-  setting {
-    namespace = "aws:elb:loadbalancer"
-    name      = "CrossZone"
-    value     = "true"  # Enable cross-zone load balancing
-  }
-
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "ELBSubnets"
-    value     = join(",", [
-      aws_subnet.public_a.id,
-      aws_subnet.public_b.id
-    ])
-  }
-
-  setting {
-    namespace = "aws:elb:loadbalancer"
-    name      = "SecurityGroups"
-    value     = aws_security_group.elb.id
-  }
-
 }
